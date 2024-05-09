ec6ab1d68e886e4aac9058550a67229614d96438
Евгений Колодько <evgeniykolodko@gmail.com>
2024-05-09 12:14:39 +0300

    add validator

--- currency.py
+++ currency.py
@@ -5,10 +5,18 @@ from db import Currency, session
 from openpyxl import Workbook
 
 
+def validate_exchange_rate(exchange_rate):
+    try:
+        float(exchange_rate)
+        return True
+    except ValueError:
+        return False
+
+
 async def process_html(html_text):
     soup = BeautifulSoup(html_text, 'html.parser')
-    exchange_rate = soup.find('div', {'data-target': 'UAH'})['data-last-price']
-    return str(round(float(exchange_rate), 4))
+    exchange_rate = soup.find('div', {'data-target': 'UAH'}).get('data-last-price')
+    return str(round(float(exchange_rate), 4)) if validate_exchange_rate(exchange_rate) else None
 
 
 async def make_answer():
@@ -19,10 +27,13 @@ async def make_answer():
             html_text = await response.text()
             exchange_rate = await process_html(html_text)
             datetime_now = datetime.now()
-            currency = Currency(datetime=datetime_now, exchange_rate=exchange_rate)
-            session.add(currency)
-            session.commit()
-            return datetime_now, exchange_rate
+            if exchange_rate:
+                currency = Currency(datetime=datetime_now, exchange_rate=exchange_rate)
+                session.add(currency)
+                session.commit()
+                return datetime_now, exchange_rate
+            else:
+                return None, None
 
 
 async def generate_excel():
@@ -34,11 +45,11 @@ async def generate_excel():
 
     today_date = date.today()
 
-    rows = session.query(Currency).filter(Currency.datetime == today_date).all()
+    rows = session.query(Currency).filter(Currency.datetime >= today_date).all()
 
     for idx, row in enumerate(rows, start=2):
         ws[f'A{idx}'] = row.datetime.strftime('%d.%m.%Y %H:%M:%S')
-        ws[f'B{idx}'] = row.kurs
+        ws[f'B{idx}'] = row.exchange_rate
 
     excel_filename = f'exchange_rate_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx'
     wb.save(excel_filename)
